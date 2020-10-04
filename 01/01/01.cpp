b3e5c770188d55ce6fe0a15011519783b29b827c
zqg <zqg.qq.com>
2020-10-04 21:21:16 +0800

    add

--- 01/01/01.cpp
+++ 01/01/01.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 #define PRICE 50 //定义常量
 
@@ -27,7 +28,39 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << z / m << endl; //2
 
 	// 最小的运算单位 为int ; char short 运算时会自动转换为 Int
-	int d = 5;
+
+	short i = 50000; //short 最大值为 32767 最小值 -32768
+
+	int i1 = 10;
+	i1 *= 5; // i1=i1*5
+
+	x = 0, y = 0;
+	x += 2, y += 2;
+	q = ((x++), (y++));  // q=2
+	cout << q << endl;
+	
+
+	cout << fixed << setprecision(2); // 设置小数点显示位数为2 需要头文件为 iomanip 即为 #include<iomanip>
+	cout << 2.567 << endl;  //2.57
+
+
+	cout << !(18==19)<<endl; //1 
+	bool flag = true;
+
+	// 逻辑运算优先级  !> && > ||  
+	flag = !1 || 2 && 2;
+	cout << flag << endl; //1
+
+
+	switch (flag)
+	{
+		case true:
+			cout << "true" << endl;
+			break;
+	default:
+		cout << "false" << endl;
+		break;
+	}
 
 
 
