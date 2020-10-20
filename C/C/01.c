7bda2b7147b91be26074750dd1491935638a590b
zqg <zqg.qq.com>
2020-10-20 23:29:02 +0800

    add

--- C/C/01.c
+++ C/C/01.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 	printf("hello world");
+	// c语言编译 需要经过 预处理  转换为汇编 --> 二进制文件 -->将C语言库链接到可执行程序文件中
+	system("calc"); // 需要 用 #include “<stdlib.h>”; 
 	return 0;
 }
