862a250f95e530cb0c2e30c6b44e7c924547b486
zqg <zqg.qq.com>
2020-10-21 23:37:17 +0800

    add

--- C/C/01.c
+++ C/C/01.c
@@ -5,5 +5,17 @@ int main(){
 	printf("hello world");
 	// c语言编译 需要经过 预处理  转换为汇编 --> 二进制文件 -->将C语言库链接到可执行程序文件中
 	system("calc"); // 需要 用 #include “<stdlib.h>”; 
+
+	int a, b, c;
+	int d = 10;
+	//嵌套汇编  f9断点
+	__asm{
+		mov a,10
+		mov b,20
+		mov eax ,a
+		add eax,b
+		mov c,eax
+	}
+	printf("%d", c);
 	return 0;
 }
