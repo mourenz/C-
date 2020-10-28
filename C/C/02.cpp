893373469d43828d25f3cbc50e79da78e84e67db
zqg <zqg.qq.com>
2020-10-28 23:48:20 +0800

    add

--- C/C/02.cpp
+++ C/C/02.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include<math.h>
 #include<iostream>
+#include<time.h>
 using namespace std;
 #pragma warning(disable:4996)  // 忽略 scanf  unsafe
 
@@ -45,11 +46,23 @@ int main(){
 	for (int i = 1; i <= 999; i++){
 		int a = i % 10;
 		int b = i / 10 % 10;
-		int c = i /100 % 10;
+		int c = i / 100 % 10;
 		if (pow(a, 3) + pow(b, 3) + pow(c, 3) == i){
 			cout << i << endl;
 		}
 	}
+
+	cout << "随机数" << endl;
+	//需要包含头文件 time.h
+	srand((unsigned int)time(NULL));// 保证每次随机数不一样
+	for (size_t i = 0; i < 10; i++)
+	{
+		int value = rand()%10; //产生 0-9的随机数
+		cout << value << endl;
+	}
+
+
+
 
 
 
